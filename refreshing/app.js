6e7f3b3567f2d0d31b72033bbae3d24ebf92069e
elsporko <elsporko@gmail.com>
2015-07-10 17:52:04 -0400

    Added concept of accounts.

--- refreshing/app.js
+++ refreshing/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var goals = require('./routes/goals');
+var accounts = require('./routes/accounts');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/goals', goals);
+app.use('/accounts', accounts);
 
 var server = app.listen(3000, function () {
 
--- refreshing/routes/index.js
+++ refreshing/routes/index.js
@@ -1,14 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Refreshing' });
 });
 
+router.use(bodyParser.urlencoded({extended: true }));
+
 /* POST home page. */
+router.post('/goalName/:goalName/goalDate/:goalDate/goalAmount/:goalAmount', function (req, res, next) {
+  console.log('~~~ specific params ~~~');
+  console.log('Goal Name => ' + goalName);
+});
+
 router.post('/', function(req, res, next) {
+  var goalName = req.body.goalName;
+
+  console.log('~~~ catchall ~~~');
+  console.log(req.body);
+  console.log('Goal Name => ' + goalName);
+/*
+  console.log('params => ' + JSON.stringify(req.params));
+*/
   res.render('index', { title: 'Refreshing' });
+  next;
 });
 
 module.exports = router;
